309393e310adfa37ef053dffaff5c79e13b8cc5e
Aurora Moreno Lopez <aurora.moreno.lopez@tajamar365.com>
2020-11-19 19:47:07 +0100

    inicio

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+import logo from './../../logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <div>
+    <h1>Examen</h1>
+    </div>
+  );
+}
+
+export default App;
